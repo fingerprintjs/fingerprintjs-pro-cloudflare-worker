aa318ba18e87323bb10dd671f7324f720154f1cd
Sergey Shelomentsev <sergey.v.shelomentsev@gmail.com>
2022-04-07 23:00:11 +0700

    Add build

---
+++ dist/fingerprintjs-pro-cloudflare-worker.d.ts
@@ -0,0 +1,16 @@
+/**
+ * FingerprintJS Pro React v0.0.1 - Copyright (c) FingerprintJS, Inc, 2022 (https://fingerprintjs.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+
+declare type WorkerEnv = {
+    AGENT_SCRIPT_DOWNLOAD_PATH: string | null;
+    VISITOR_ID_PATH: string | null;
+    WORKER_PATH: string | null;
+};
+
+declare const _default: {
+    fetch(request: Request, env: WorkerEnv): Promise<Response>;
+};
+
+export { _default as default };
