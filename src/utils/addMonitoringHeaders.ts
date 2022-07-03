f38ddd436db2879b62ac6eef497bca3ddee39e59
Necip Allef <necipallef@gmail.com>
2022-07-04 00:24:00 +0300

    feat -pro infix

--- src/utils/addMonitoringHeaders.ts
+++ src/utils/addMonitoringHeaders.ts
@@ -2,7 +2,7 @@ const INT_VERSION = '__current_worker_version__'
 const HEADER_NAME = 'ii'
 
 function getHeaderValue(type: 'procdn' | 'ingress'): string {
-  return `fingerprintjs-cloudflare/${INT_VERSION}/${type}`
+  return `fingerprintjs-pro-cloudflare/${INT_VERSION}/${type}`
 }
 
 export function addMonitoringHeadersForProCDN(url: URL) {
