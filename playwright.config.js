5a867411dc4f29cbf40d2a849c887aa06ae3a473
necipallef <necipallef@gmail.com>
2022-07-13 18:54:19 +0300

    Update and rename playwright.config.js to playwright.config.ts

--- playwright.config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  testDir: './test/e2e',
-};
-
-export default config;
---
+++ playwright.config.ts
@@ -0,0 +1,8 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './test/e2e',
+};
+
+export default config;
