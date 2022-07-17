831a21678069cbf3f5c003901f46da136486a33e
necipallef <necipallef@gmail.com>
2022-07-18 00:29:18 +0300

    Update visitorId.spec.ts

--- test/e2e/visitorId.spec.ts
+++ test/e2e/visitorId.spec.ts
@@ -10,7 +10,7 @@ const GET_RESULT_PATH = process.env.get_result_path || 'get-result-default'
 // @ts-ignore
 const AGENT_DOWNLOAD_PATH = process.env.agent_download_path || 'agent-download-default'
 
-const npmWebsiteURL = `https://automated-test-client.cfi-fingerprint.com?worker-path=${WORKER_PATH}&get-result-path=${GET_RESULT_PATH}&agent-path=${AGENT_DOWNLOAD_PATH}` // todo use URL constructor and searchParams
+const npmWebsiteURL = `https://automated-test-client.cfi-fingerprint.com?worker-path=${WORKER_PATH}&get-result-path=${GET_RESULT_PATH}&agent-download-path=${AGENT_DOWNLOAD_PATH}` // todo use URL constructor and searchParams
 console.log({npmWebsiteURL})
 const workerDomain = 'https://automated-test-client.cfi-fingerprint.com'
 
