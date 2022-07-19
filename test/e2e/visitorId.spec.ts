5e7ebcc3c33cc41a48ddf44cf0e16bdbe4065744
necipallef <necipallef@gmail.com>
2022-07-19 17:35:54 +0300

    test refactor (#35)

--- test/e2e/visitorId.spec.ts
+++ test/e2e/visitorId.spec.ts
@@ -10,9 +10,8 @@ const GET_RESULT_PATH = process.env.get_result_path || 'get-result-default'
 // @ts-ignore
 const AGENT_DOWNLOAD_PATH = process.env.agent_download_path || 'agent-download-default'
 
-const npmWebsiteURL = `https://automated-test-client.cfi-fingerprint.com?worker-path=${WORKER_PATH}&get-result-path=${GET_RESULT_PATH}&agent-download-path=${AGENT_DOWNLOAD_PATH}` // todo use URL constructor and searchParams
-console.log({npmWebsiteURL})
-const workerDomain = 'https://automated-test-client.cfi-fingerprint.com'
+const workerDomain = `https://${process.env.test_client_domain}`
+const npmWebsiteURL = `${workerDomain}?worker-path=${WORKER_PATH}&get-result-path=${GET_RESULT_PATH}&agent-download-path=${AGENT_DOWNLOAD_PATH}` // todo use URL constructor and searchParams
 
 interface GetResult {
   requestId: string
@@ -58,7 +57,6 @@ test.describe('visitorId', () => {
     const el = await page.waitForSelector('#result > code')
     const textContent = await el.textContent()
     expect(textContent != null).toStrictEqual(true)
-    console.log({textContent})
     let jsonContent
     try {
       jsonContent = JSON.parse(textContent as string)
