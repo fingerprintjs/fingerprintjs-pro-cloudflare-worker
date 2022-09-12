a1dc32ff9ba85b0707886ff355a16e1ba7e60609
Necip Allef <necipallef@gmail.com>
2022-09-12 11:36:55 +0300

    refactor: rename func

--- src/utils/addTrafficMonitoring.ts
+++ src/utils/addTrafficMonitoring.ts
@@ -1,14 +1,14 @@
 const INT_VERSION = '__current_worker_version__'
 const HEADER_NAME = 'ii'
 
-function getHeaderValue(type: 'procdn' | 'ingress'): string {
+function getValueByType(type: 'procdn' | 'ingress'): string {
   return `fingerprintjs-pro-cloudflare/${INT_VERSION}/${type}`
 }
 
 export function addTrafficMonitoringSearchParamsForProCDN(url: URL) {
-  url.searchParams.append(HEADER_NAME, getHeaderValue('procdn'))
+  url.searchParams.append(HEADER_NAME, getValueByType('procdn'))
 }
 
 export function addTrafficMonitoringSearchParamsForVisitorIdRequest(url: URL) {
-  url.searchParams.append(HEADER_NAME, getHeaderValue('ingress'))
+  url.searchParams.append(HEADER_NAME, getValueByType('ingress'))
 }
