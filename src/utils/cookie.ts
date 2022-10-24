ffcc30661a83e7265ced83bd90bf49e9396e8894
Necip Allef <necipallef@gmail.com>
2022-10-24 11:12:22 +0300

    refactor: cookie functions moved to one file

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -2,3 +2,28 @@ export type Cookie = {
   value: string
   [key: string]: string
 }
+
+export function createCookieObjectFromHeaderValue(cookieValue: string): [string, Cookie] {
+  let cookieName: string = ''
+  const cookieObject: Cookie = cookieValue.split('; ').reduce(
+    (prev: Cookie, flag: string, index: number) => {
+      const kv = flag.split('=')
+      const key = index === 0 ? 'value' : kv[0]
+      if (index === 0) {
+        cookieName = kv[0]
+      }
+      const value = kv[1]
+      return { ...prev, [key]: value }
+    },
+    { value: '' },
+  )
+
+  return [cookieName, cookieObject]
+}
+
+export function createCookieStringFromObject(name: string, value: Cookie) {
+  const flags = Object.entries(value).filter(([k]) => k !== name && k !== 'value')
+  const nameValue = `${name}=${value.value}`
+  const rest = flags.map(([k, v]) => (v ? `${k}=${v}` : k))
+  return [nameValue, ...rest].join('; ')
+}
