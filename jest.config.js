0e09ecaaf5476377d2e7210b39b5d2967237f758
Necip Allef <necipallef@gmail.com>
2022-11-07 14:46:54 +0300

    test: remove jest.setup.js

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   testEnvironment: 'miniflare',
   testRegex: '/test/.+test.tsx?$',
   passWithNoTests: true,
-  setupFiles: ['./jest.setup.js'],
   collectCoverageFrom: ['./src/utils/**.ts'],
   coverageReporters: ['lcov', 'json-summary', ['text', { file: 'coverage.txt', path: './' }]],
 }
