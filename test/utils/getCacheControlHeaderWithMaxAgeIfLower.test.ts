f6ca1f143baddd44262894fd4861e5b4d1fce147
Necip Allef <necipallef@gmail.com>
2022-11-14 15:59:20 +0300

    fix: s-maxage also being set to lower values, like max-age

--- test/utils/getCacheControlHeaderWithMaxAgeIfLower.test.ts
+++ test/utils/getCacheControlHeaderWithMaxAgeIfLower.test.ts
@@ -3,12 +3,21 @@ import { getCacheControlHeaderWithMaxAgeIfLower } from '../../src/utils'
 describe('getCacheControlHeaderWithMaxAgeIfLower', () => {
   const f = getCacheControlHeaderWithMaxAgeIfLower
   test('if maxAge < maxMaxAge then use maxAge', () => {
-    expect(f('public, max-age=3600, s-maxage=633059', 1200)).toBe('public, max-age=1200, s-maxage=633059')
+    expect(f('public, max-age=3600, s-maxage=633059', 1200)).toBe('public, max-age=1200, s-maxage=1200')
   })
   test('if maxAge > maxMaxAge then use maxMaxAge', () => {
-    expect(f('public, max-age=3600, s-maxage=633059', 6000)).toBe('public, max-age=3600, s-maxage=633059')
+    expect(f('public, max-age=3600, s-maxage=633059', 6000)).toBe('public, max-age=3600, s-maxage=6000')
   })
   test('if maxAge is absent then use maxMaxAge', () => {
-    expect(f('public', 6000)).toBe('public, max-age=6000')
+    expect(f('public', 6000)).toBe('public, max-age=6000, s-maxage=6000')
+  })
+  test('if s-maxAge < maxMaxAge then use maxAge', () => {
+    expect(f('public, max-age=3600, s-maxage=3600', 1200)).toBe('public, max-age=1200, s-maxage=1200')
+  })
+  test('if s-maxAge > maxMaxAge then use maxMaxAge', () => {
+    expect(f('public, max-age=3600, s-maxage=3600', 6000)).toBe('public, max-age=3600, s-maxage=3600')
+  })
+  test('if s-maxAge is absent then use maxMaxAge', () => {
+    expect(f('public', 6000)).toBe('public, max-age=6000, s-maxage=6000')
   })
 })
