25364e30594c7464d8bbf228c8e484ad34a92bd9
Necip Allef <necipallef@gmail.com>
2023-01-03 21:56:41 +0300

    feat: replace primitive routing with a proper one

--- src/utils/routing.ts
+++ src/utils/routing.ts
@@ -1,3 +1,33 @@
 export function removeTrailingSlashes(str: string): string {
   return str.replace(/\/+$/, '')
 }
+
+function addTrailingWildcard(str: string): string {
+  return str.replace(/(\/?)\*/g, '($1.*)?')
+}
+
+function removeTrailingSlashAndDoubleSlash(str: string): string {
+  return str.replace(/(\/$)|((?<=\/)\/)/, '')
+}
+
+function makeGreedy(str: string): string {
+  return str.replace(/(:(\w+)\+)/, '(?<$2>.*)')
+}
+
+function replaceNamedParams(str: string): string {
+  return str.replace(/:(\w+)(\?)?(\.)?/g, '$2(?<$1>[^/]+)$2$3')
+}
+
+function replaceDot(str: string): string {
+  return str.replace(/\.(?=[\w(])/, '\\.')
+}
+
+export function createRoute(route: string): RegExp {
+  let routeRegExp = route
+  routeRegExp = addTrailingWildcard(routeRegExp)
+  routeRegExp = removeTrailingSlashAndDoubleSlash(routeRegExp)
+  routeRegExp = makeGreedy(routeRegExp)
+  routeRegExp = replaceNamedParams(routeRegExp)
+  routeRegExp = replaceDot(routeRegExp)
+  return RegExp(`^${routeRegExp}/*$`)
+}
