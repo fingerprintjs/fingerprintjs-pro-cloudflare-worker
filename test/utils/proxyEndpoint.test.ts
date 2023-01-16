96bda98afa5396b14b07c45b117858eaf9c77012
Necip Allef <necipallef@gmail.com>
2023-01-16 22:53:13 +0300

    test: getAgentScriptEndpoint and getVisitorIdEndpoint tests

--- test/utils/proxyEndpoint.test.ts
+++ test/utils/proxyEndpoint.test.ts
@@ -1,19 +1,47 @@
 import { getAgentScriptEndpoint, getVisitorIdEndpoint } from '../../src/utils'
 
 describe('getAgentScriptEndpoint', () => {
-  const apiKey = 'generatedApiKey'
+  const f = getAgentScriptEndpoint
+  const apiKey = 'randomlyGeneratedApiKey'
 
   test('apiKey exists, version does not exist', () => {
     const urlSearchParams = new URLSearchParams()
     urlSearchParams.set('apiKey', apiKey)
-    expect(getAgentScriptEndpoint(urlSearchParams)).toBe(`https://fpcdn.io/v3/${apiKey}`)
+    expect(f(urlSearchParams)).toBe(`https://fpcdn.io/v3/${apiKey}`)
   })
-  // todo more tests
+  test('apiKey exists, version exists', () => {
+    const version = '4'
+    const urlSearchParams = new URLSearchParams()
+    urlSearchParams.set('apiKey', apiKey)
+    urlSearchParams.set('version', version)
+    expect(f(urlSearchParams)).toBe(`https://fpcdn.io/v${version}/${apiKey}`)
+  })
+  test('apiKey exists, version does not exist, loaderVersion exists', () => {
+    const loaderVersion = '3.7.0'
+    const urlSearchParams = new URLSearchParams()
+    urlSearchParams.set('apiKey', apiKey)
+    urlSearchParams.set('loaderVersion', loaderVersion)
+    expect(f(urlSearchParams)).toBe(`https://fpcdn.io/v3/${apiKey}/loader_v${loaderVersion}.js`)
+  })
+  test('apiKey exists, version exists, loaderVersion exists', () => {
+    const version = '4'
+    const loaderVersion = '3.7.0'
+    const urlSearchParams = new URLSearchParams()
+    urlSearchParams.set('apiKey', apiKey)
+    urlSearchParams.set('version', version)
+    urlSearchParams.set('loaderVersion', loaderVersion)
+    expect(f(urlSearchParams)).toBe(`https://fpcdn.io/v${version}/${apiKey}/loader_v${loaderVersion}.js`)
+  })
 })
 
 describe('getVisitorIdEndpoint', () => {
   test('us region', () => {
     expect(getVisitorIdEndpoint('us')).toBe('https://api.fpjs.io')
   })
-  // todo more tests
+  test('eu region', () => {
+    expect(getVisitorIdEndpoint('eu')).toBe('https://eu.api.fpjs.io')
+  })
+  test('ap region', () => {
+    expect(getVisitorIdEndpoint('ap')).toBe('https://ap.api.fpjs.io')
+  })
 })
