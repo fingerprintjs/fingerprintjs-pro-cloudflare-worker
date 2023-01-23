baf7a4394a7409317305489ca2eed1c72a295791
Necip Allef <necipallef@gmail.com>
2023-01-23 19:10:57 +0300

    test: fix a case of cache control

--- test/utils/getCacheControlHeaderWithMaxAgeIfLower.test.ts
+++ test/utils/getCacheControlHeaderWithMaxAgeIfLower.test.ts
@@ -9,7 +9,7 @@ describe('getCacheControlHeaderWithMaxAgeIfLower', () => {
     expect(f('public, max-age=3600, s-maxage=633059', 6000, 100)).toBe('public, max-age=3600, s-maxage=100')
   })
   test('if maxAge is absent then use maxMaxAge', () => {
-    expect(f('public', 6000, 100)).toBe('public, max-age=6000, s-maxage=60')
+    expect(f('public', 6000, 100)).toBe('public, max-age=6000, s-maxage=100')
   })
   test('if s-maxAge < maxSMaxAge then use maxSMaxAge', () => {
     expect(f('public, max-age=3600, s-maxage=3600', 1200, 1200)).toBe('public, max-age=1200, s-maxage=1200')
