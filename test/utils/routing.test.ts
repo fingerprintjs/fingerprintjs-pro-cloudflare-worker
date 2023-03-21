87764d29ebce8d56f71feb7c7dff5328fa4e2133
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2023-03-21 15:12:41 +0300

    fix(routing): support more characters for createRoute

--- test/utils/routing.test.ts
+++ test/utils/routing.test.ts
@@ -1,4 +1,5 @@
-import { removeTrailingSlashesAndMultiSlashes, addTrailingWildcard, replaceDot } from '../../src/utils'
+import {removeTrailingSlashesAndMultiSlashes, addTrailingWildcard, replaceDot, createRoute} from '../../src/utils'
+import {addEndingTrailingSlashToRoute, addPathnameMatchBeforeRoute} from "../../src/utils/routing";
 
 describe('removeTrailingSlashesAndMultiSlashes', () => {
   it('returns /path for /path', () => {
@@ -57,5 +58,30 @@ describe('replaceDot', () => {
   })
   it('returns empty string for empty string', () => {
     expect(replaceDot('')).toBe('')
+  })
+})
+
+describe('addEndingTrailingSlashToRoute', () => {
+  it('should add \\/* to /status', () => {
+    expect(addEndingTrailingSlashToRoute('/status')).toBe('/status\\/*');
+  })
+})
+
+describe('pathNameRegExp', () => {
+  it('should add [\\/[A-Za-z0-9:._-]* to /status', () => {
+    expect(addPathnameMatchBeforeRoute('/status')).toBe('[\\/[A-Za-z0-9:._-]*/status')
+  })
+  it('return true for /path/path2/example-path-2/status when tested', () => {
+    expect(
+      RegExp(`^${addPathnameMatchBeforeRoute(removeTrailingSlashesAndMultiSlashes('/status'))}$`).test(
+        '/path/path2/example-path-2/status',
+      ),
+    ).toBe(true)
+  })
+})
+
+describe('createRoute', () => {
+  it.each(['/fpjs-worker-path-0123456789/status', '/fpjsworker/status', '/status', '/path/path2/path3/path4/status'])('%s should match with /status', (route) => {
+    expect(createRoute('/status').test(route)).toBe(true)
   })
 })
