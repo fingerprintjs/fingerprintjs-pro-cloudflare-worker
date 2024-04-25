d64e77bab93f4b6c1ff15e11a3a21ea39af37849
Necip Allef <necipallef@gmail.com>
2024-04-25 13:25:54 +0300

    test: test ci

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { handleRequest } from './handler'
 import { WorkerEnv } from './env'
 import { returnHttpResponse } from './utils'
 
+console.log('test')
 export default {
   async fetch(request: Request, env: WorkerEnv) {
     return handleRequest(request, env).then(returnHttpResponse)
