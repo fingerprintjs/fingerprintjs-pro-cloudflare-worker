1d045c37783ab7b4a54b2bd951370721360b9263
Necip Allef <necipallef@gmail.com>
2025-02-25 17:45:19 +0300

    chore: testing out different implementations of getIP

--- src/utils/addProxyIntegrationHeaders.ts
+++ src/utils/addProxyIntegrationHeaders.ts
@@ -1,5 +1,17 @@
 import { getProxySecret, WorkerEnv } from '../env'
 
+function getIPFromHeaders(headers: Headers){
+  if (headers.get('Cf-Pseudo-IPv4') === headers.get('CF-Connecting-IP')) {
+    return headers.get('CF-Connecting-IPv6') || ''
+  }
+
+  return headers.get('CF-Connecting-IP') || ''
+}
+
+function getIPFromHeaders2(headers: Headers){
+  return headers.get('CF-Connecting-IPv6') || headers.get('CF-Connecting-IP') || ''
+}
+
 export function addProxyIntegrationHeaders(headers: Headers, url: string, env: WorkerEnv) {
   const proxySecret = getProxySecret(env)
   if (proxySecret) {
