436e06b6eb5b8493521af6053f403c09137d5d0b
Eray Aydın <eray.aydin@fingerprint.com>
2025-03-10 13:55:16 +0300

    chore: add comment about handling CloudFlare's Pseudo IPv4 feature in getIPFromHeaders
    Added a comment to the `getIPFromHeaders` function to explain the
    handling of CloudFlare's Pseudo IPv4 feature.
    
    Co-Authored-By: @JuroUhlar

--- src/utils/addProxyIntegrationHeaders.ts
+++ src/utils/addProxyIntegrationHeaders.ts
@@ -14,6 +14,7 @@ export function addProxyIntegrationHeaders(headers: Headers, url: string, env: W
 export function getIPFromHeaders(headers: Headers) {
   const connectingIP = headers.get('CF-Connecting-IP')
 
+  // Correctly handle CloudFlare's [Pseudo IPv4](https://developers.cloudflare.com/network/pseudo-ipv4/) feature set to `Overwrite headers`
   if (headers.get('Cf-Pseudo-IPv4') === connectingIP) {
     return headers.get('CF-Connecting-IPv6') || ''
   }
